dbd636f603d163df3d3d2545a373f217c2d25f6c
RusUsf <ruslan@usf.edu>
2023-09-09 17:06:08 -0400

    Practiced 1.T13

--- PracticeCode/Program.cs
+++ PracticeCode/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1.T13 Decimal vs double vs float in finances
+var b = 6;
+decimal flag1 = (decimal)10.0 / b;
+double flag2 = 10.0 / b;
+float flag3 = (float)10.0 / b;
+
+Console.WriteLine(flag1);
+Console.WriteLine(flag2);
+Console.WriteLine(flag3);
+
+var flag5 = false;
+flag3 = flag3 + 3;
+flag1 = flag1 + 6;
+
+//1.T14 conditional operator is right-associative (evaluated from right to left)
+var c = b == 6 ? (int)flag3 : flag5 ? (int)flag3 : (int)flag1;
+Console.WriteLine(c);
+var c1 = b > 6 ? (int)flag3 : (flag5 ? (int)flag3 : (int)flag1);
+Console.WriteLine(c1);
+
+
