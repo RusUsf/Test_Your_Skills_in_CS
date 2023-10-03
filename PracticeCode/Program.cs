f90c85f9780332a564cf4795cb54f44e5b350606
RusUsf <ruslan@usf.edu>
2023-10-03 10:22:00 -0400

    Worked on 6.P1

--- PracticeCode/Program.cs
+++ PracticeCode/Program.cs
@@ -1523,6 +1523,33 @@
 //    }
 //}
 
+//// Chapter 6 Programming Quesitions
+
+//// 6.P1
+//Console.WriteLine("Understanding the new Keyword in C#.");
+//Parent parent = new Parent();
+//Console.WriteLine(parent.GetNumber(5));
+//Console.WriteLine(parent.GetNumber(5,7));
+
+//Console.WriteLine("----");
+//parent = new Child();
+//Console.WriteLine(parent.GetNumber(5));
+//Console.WriteLine(parent.GetNumber(5,7)); // still uses original method in Parent class
+
+//Child child = new Child();
+//Console.WriteLine(child.GetNumber(5,7)); // uses new method in the Child class
+//class Parent
+//{
+//    internal virtual int GetNumber(int i) => i;
+//    public virtual int GetNumber(int i, int j) => i + j;
+//}
+
+//class Child : Parent
+//{
+//    internal override int GetNumber(int i) => i + 2;
+//    internal new int GetNumber(int i, int j) => i + j + 15;
+//}
+
 
 
 
