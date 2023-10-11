222103c3a6ff87ab4eee2b19b0950737fbe9a85a
RusUsf <ruslan@usf.edu>
2023-10-11 16:08:03 -0400

    Finished Chapter 7

--- PracticeCode/Program.cs
+++ PracticeCode/Program.cs
@@ -2511,7 +2511,22 @@
 //    }
 //}
 
-
+//// 7.P17
+//Rectangle rect = new(5.5, 10);
+//Console.WriteLine($"The area of the rectangle is {rect.Area}");
+//class Rectangle
+//{
+//    double length, breadth;
+//    public Rectangle(double length, double breadth)
+//    {
+//        this.length = length;
+//        this.breadth = breadth;
+//    }
+//    public double Area
+//    {
+//        get => length * breadth;
+//    }
+//}
 
 
 
