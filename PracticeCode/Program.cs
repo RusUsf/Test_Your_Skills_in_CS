adfbadb89f134c2a19b0066e9c4153cf6f4924d0
RusUsf <ruslan@usf.edu>
2023-10-16 19:46:51 -0400

    Finished Chapter 9

--- PracticeCode/Program.cs
+++ PracticeCode/Program.cs
@@ -2735,7 +2735,80 @@
 //    }
 //}
 
+//// 8.P8 Throw Ex
+//Console.WriteLine("***The case study on the trow statement.***");
+//NumberMaker numberMaker = new();
+//try
+//{
+//    Console.WriteLine($"numbers[0] = {numberMaker.GetNumber(0)}");
+//    Console.WriteLine($"numbers[3] = {numberMaker.GetNumber(3)}");
+//}
+//catch (Exception ex)
+//{
+//    Console.WriteLine($"\nThe Main() caught: {ex.Message}");
+//    Console.WriteLine($"\tStacktrace: {ex.StackTrace}");
+//}
+//class NumberMaker
+//{
+//    int[] _numbers = { 1, 2, 3 };
 
+//    internal int GetNumber(int index)
+//    {
+//        try
+//        {
+//            if (index < 0 || index >= _numbers.Length)
+//            {
+//                throw new IndexOutOfRangeException("Index is out of range.");
+//            }
+//        }
+//        catch (Exception ex)
+//        {
+//            Console.WriteLine($"\nThe GetNumber(...) caught:{ex.Message}");
+//            Console.WriteLine($"\tStacktrace: {ex.Message}");
+//            throw ex;
+//        }
+//        return _numbers[index];
+//    }
+//}
+
+//// 8.P9
+//Console.WriteLine("***The case study on the throw expression.***");
+//try
+//{
+//    int a = 10;
+//    // Returns a random number between 0 (inclusive) and 2 (exclusive)
+//    int b = new Random().Next(0, 2);
+//    Console.WriteLine($"b = {b}");
+//    int c = b > 0 ? (a / b) :
+//        throw new DivideByZeroException("b becomes 0");
+//    Console.WriteLine("Thank you.");
+//}
+//catch(Exception ex)
+//{
+//    Console.WriteLine($"Caught:"+ex.Message );
+//}
+
+//// 8.P10
+//using System.Net;
+//Console.WriteLine("***A case study on exception filters.***");
+//try
+//{
+//    int a = 10;
+//    // Returns a random number between 0 (inclusive) and 2 (exclusive)
+//    int b = new Random().Next(0, 2);
+//    Console.WriteLine($"b = {b}");
+//    int c = b > 0 ? throw new WebException("Timeout error") :
+//        throw new WebException("Protocol error");
+//    Console.WriteLine("Thank you.");
+//}
+//catch (Exception ex) when (ex.Message.Contains("Timeout"))
+//{
+//    Console.WriteLine($"Caught:" + ex.Message);
+//}
+//catch (Exception ex) when (ex.Message.Contains("Protocol"))
+//{
+//    Console.WriteLine($"Cought:" + ex.Message);
+//}
 
 
 
