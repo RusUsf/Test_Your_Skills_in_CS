29d22c187f71c2c23f1289452c3f6555cdb5787d
Ruslan Dubas <rdubas@onmed.com>
2023-10-16 10:29:53 -0400

    Worked on 8.P7

--- PracticeCode/Program.cs
+++ PracticeCode/Program.cs
@@ -2699,7 +2699,41 @@
 //    Console.WriteLine($"The exception caught: {ex.Message}");
 //}
 
+//// 8.P7
+//Console.WriteLine("***The case study on the trow statement.***");
+//NumberMaker numberMaker = new();
+//try
+//{
+//    Console.WriteLine($"numbers[0] = {numberMaker.GetNumber(0)}");
+//    Console.WriteLine($"numbers[3] = {numberMaker.GetNumber(3)}");
+//}
+//catch (Exception ex)
+//{
+//    Console.WriteLine($"\nThe Main() caught: {ex.Message}");
+//    Console.WriteLine($"\tStacktrace: {ex.StackTrace}");
+//}
+//class NumberMaker
+//{
+//    int[] _numbers = { 1, 2, 3 };
 
+//    internal int GetNumber(int index)
+//    {
+//        try
+//        {
+//            if (index < 0 || index >= _numbers.Length)
+//            {
+//                throw new IndexOutOfRangeException("Index is out of range.");
+//            }
+//        }
+//        catch (Exception ex)
+//        {
+//            Console.WriteLine($"\nThe GetNumber(...) caught:{ex.Message}");
+//            Console.WriteLine($"\tStacktrace: {ex.Message}");
+//            throw;
+//        }
+//        return _numbers[index];
+//    }
+//}
 
 
 
