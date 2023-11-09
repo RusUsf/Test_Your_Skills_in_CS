3a019d772fbb981459f91cfb37ca15b547d83484
Ruslan <ruslan@usf.edu>
2023-11-08 21:14:01 -0500

    Finished Theoretical Questions in chapter 10

--- PracticeCode/Program.cs
+++ PracticeCode/Program.cs
@@ -3144,6 +3144,18 @@
 //    }
 //}
 
+//// 10.T14
+//Predicate<int> isGreater = Sample.TestGreaterThan50;
+//Console.WriteLine($"51 is greater than 50?{isGreater(51)}");
+//Console.WriteLine($"49 is greater than 50?{isGreater(49)}");
+//class Sample
+//{
+//    public static bool TestGreaterThan50(int input)
+//    {
+//        return input > 50;
+//    }
+//}
+
 
 
 
